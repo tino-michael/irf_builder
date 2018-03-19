ccbd917293da96e39494c4a7ba35a27c5fb2b79a
@tino-michael <tino.michael@cea.fr>
2018-03-19 16:51:04 +0100

    started on toy mc generator

---
+++ irf_builder/pseudo_simulation/__init__.py
@@ -0,0 +1,29 @@
+from scipy import interpolate
+
+import irf_builder as irf
+
+
+def draw_from_distribution(dist, abscis, n_draws=100, k=1):
+    """draws events from a given distribution
+
+    Parameters
+    ----------
+    dist : 1d array
+        the distribution the drawn events are supposed to follow
+    abscis : 1d array
+        the abscissa values corresponding to `dist`
+    n_draws : integer (default: 100)
+        number of samples to be drawn
+    k : integer (default: 1)
+        degree of spline interpolation of the cumulative distribution of `dist`
+
+    Returns
+    -------
+    randomx : 1d array
+        array of samples randomly drawn from the provided distribution
+
+    """
+    cdf = np.cumsum(dist)
+    cdfspline = interpolate.splrep(cdf, abscis, k=1)
+    randomx = interpolate.splev(np.random.uniform(*abscis[[0, -1]], n_draws), cdfspline)
+    return randomx
